af2c749a27b1362f56133c65b14460b761c08c83
Natalia Rusu <bonapartik@gmail.com>
2022-10-05 19:31:00 +0100

    fix start, stop and reset handlers

--- index.js
+++ index.js
@@ -1,73 +1,76 @@
-const timerBlock=document.querySelectorAll('span');
+const timeSpans=document.querySelectorAll('span');
 const hoursEl=document.querySelector('#hours');
 const minutesEl=document.querySelector('#minutes');
 const secondsEl=document.querySelector('#seconds');
-const msSecondsEl=document.querySelector('#mseconds');
+const csSecondsEl=document.querySelector('#mseconds');
 const start_pause_btn=document.querySelector('.toggleTimer');
 const reset_btn=document.querySelector('.reset');
+let interval;
 
 const timer={
     hours: 0,
     minutes: 0,
     seconds: 0,
-    milliseconds: 0
+    cs_seconds: 0
 };
-let interval;
 
-const timerOn=()=>{
-    interval=setInterval(()=>{
-        timer.milliseconds+=1;        
-        if(timer.milliseconds===100){
-            timer.milliseconds=0;
-            timer.seconds+=1;            
-            secondsEl.innerHTML=timer.milliseconds<10 ?`0${timer.seconds}`:`${timer.seconds}`;
-        }
-        if(timer.seconds===60){
-            timer.seconds=0;
-            timer.minutes+=1;
-            minutesEl.innerHTML=timer.milliseconds<10 ?`0${timer.minutes}`:`0${timer.minutes}`;
-        }
-        if(timer.minutes===60){
-            timer.minutes=0;
-            timer.hours+=1;
-            hoursEl.innerHTML=timer.milliseconds<10 ?`0${timer.hours}`:`${timer.hours}`;
-        }
-        msSecondsEl.innerHTML=`${timer.milliseconds}`
-    },10);
-}
-const timerOff=()=>{
-    clearInterval(interval);
-}
 const displayStartBtn=()=>{
     start_pause_btn.classList.remove('pause');
     start_pause_btn.classList.add('start');
     start_pause_btn.innerHTML='START'; 
 }
-const displaypauseBtn=()=>{
+const displayPauseBtn=()=>{
     start_pause_btn.classList.remove('start');
     start_pause_btn.classList.add('pause');
     start_pause_btn.innerHTML='PAUSE';  
 }
 
-const startpauseHandler=()=>{
+const timerOn=()=>{
+    interval=setInterval(()=>{
+        timer.cs_seconds+=1;        
+        if(timer.cs_seconds===100){
+            timer.cs_seconds=0;
+            timer.seconds+=1;           
+            secondsEl.innerHTML=timer.seconds<10 ?`0${timer.seconds}`:`${timer.seconds}`;
+        }
+        if(timer.seconds===60){
+            timer.seconds=0;
+            timer.minutes+=1;
+            minutesEl.innerHTML=timer.minutes<10 ?`0${timer.minutes}`:`0${timer.minutes}`;
+        }
+        if(timer.minutes===60){
+            timer.minutes=0;
+            timer.hours+=1;
+            hoursEl.innerHTML=timer.hours<10 ?`0${timer.hours}`:`${timer.hours}`;
+        }
+        csSecondsEl.innerHTML=timer.cs_seconds<10?`0${timer.cs_seconds}`:`${timer.cs_seconds}`;
+    },10);
+}
+const timerOff=()=>{
+    clearInterval(interval);
+}
+
+const startPauseHandler=()=>{
     if(start_pause_btn.classList.contains('start')){//start the timer
-        displaypauseBtn();
+        displayPauseBtn();
         timerOn();
     } else {
         displayStartBtn();
         timerOff();
     } 
 }
-const resetHandler=()=>{
+const resetAllHandler=()=>{
     displayStartBtn();
-    for(const span of timerBlock){
+    timerOff();
+    for(const span of timeSpans){
         span.innerHTML="00";
     }
     for(const key in timer){
         timer[key]=0;
-    }
-    timerOff();
+    }    
+    clearLaps();
 }
+checkSavedLaps();
 
-start_pause_btn.addEventListener('click', startpauseHandler);
-reset_btn.addEventListener('click', resetHandler);
+start_pause_btn.addEventListener('click', startPauseHandler);
+reset_btn.addEventListener('click', resetAllHandler);
