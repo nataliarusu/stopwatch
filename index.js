521ce5be3f490270c5a41d359573ef193dca43b6
Natalia Rusu <bonapartik@gmail.com>
2022-10-09 21:16:54 +0100

    add split-lap feature

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-const timeSpans = document.querySelectorAll('span');
+const main_timerSpans = document.querySelectorAll('.main-timer-output span');
+const lap_timerSpans = document.querySelectorAll('.lap-timer-output span');
+
 const hoursEl = document.querySelector('#hours');
 const minutesEl = document.querySelector('#minutes');
 const secondsEl = document.querySelector('#seconds');
 const csSecondsEl = document.querySelector('#mseconds');
+const lap_hoursEl = document.querySelector('#lap_hours');
+const lap_minutesEl = document.querySelector('#lap_minutes');
+const lap_secondsEl = document.querySelector('#lap_seconds');
+const lap_csSecondsEl = document.querySelector('#lap_mseconds');
 const start_pause_btn = document.querySelector('.toggleTimer');
 const reset_btn = document.querySelector('.reset');
+
 let interval;
 
-const timer = {
+const main_timer = {
   hours: 0,
   minutes: 0,
   seconds: 0,
   cs_seconds: 0,
 };
-
+const lap_timer = {
+  hours: 0,
+  minutes: 0,
+  seconds: 0,
+  cs_seconds: 0,
+};
 const displayStartBtn = () => {
   start_pause_btn.classList.remove('pause');
   start_pause_btn.classList.add('start');
@@ -27,31 +39,46 @@ const displayPauseBtn = () => {
 
 const timerOn = () => {
   interval = setInterval(() => {
-    timer.cs_seconds += 1;
-    if (timer.cs_seconds === 100) {
-      timer.cs_seconds = 0;
-      timer.seconds += 1;
-      secondsEl.innerHTML =
-        timer.seconds < 10 ? `0${timer.seconds}` : `${timer.seconds}`;
-    }
-    if (timer.seconds === 60) {
-      timer.seconds = 0;
-      timer.minutes += 1;
-      minutesEl.innerHTML =
-        timer.minutes < 10 ? `0${timer.minutes}` : `0${timer.minutes}`;
-    }
-    if (timer.minutes === 60) {
-      timer.minutes = 0;
-      timer.hours += 1;
-      hoursEl.innerHTML =
-        timer.hours < 10 ? `0${timer.hours}` : `${timer.hours}`;
-    }
-    csSecondsEl.innerHTML =
-      timer.cs_seconds < 10 ? `0${timer.cs_seconds}` : `${timer.cs_seconds}`;
+    startTimer(main_timer, csSecondsEl, secondsEl, minutesEl, hoursEl);
+    startTimer(
+      lap_timer,
+      lap_csSecondsEl,
+      lap_secondsEl,
+      lap_minutesEl,
+      lap_hoursEl
+    );
   }, 10);
 };
+
+const startTimer = (timer, ...htmlEls) => {
+  const [csSecondsEl, secondsEl, minutesEl, hoursEl] = htmlEls;
+
+  timer.cs_seconds += 1;
+  if (timer.cs_seconds === 100) {
+    timer.cs_seconds = 0;
+    timer.seconds += 1;
+    secondsEl.innerHTML =
+      timer.seconds < 10 ? `0${timer.seconds}` : `${timer.seconds}`;
+  }
+  if (timer.seconds === 60) {
+    timer.seconds = 0;
+    timer.minutes += 1;
+    minutesEl.innerHTML =
+      timer.minutes < 10 ? `0${timer.minutes}` : `0${timer.minutes}`;
+  }
+  if (timer.minutes === 60) {
+    timer.minutes = 0;
+    timer.hours += 1;
+    hoursEl.innerHTML = timer.hours < 10 ? `0${timer.hours}` : `${timer.hours}`;
+  }
+  csSecondsEl.innerHTML =
+    timer.cs_seconds < 10 ? `0${timer.cs_seconds}` : `${timer.cs_seconds}`;
+};
+
 const timerOff = () => {
   clearInterval(interval);
+  reset_Timer(lap_timer);
+  reset_Span(lap_timerSpans);
 };
 
 const startPauseHandler = () => {
@@ -64,15 +91,24 @@ const startPauseHandler = () => {
     timerOff();
   }
 };
-const resetAllHandler = () => {
-  displayStartBtn();
-  timerOff();
-  for (const span of timeSpans) {
-    span.innerHTML = '00';
-  }
+
+const reset_Timer = (timer) => {
   for (const key in timer) {
     timer[key] = 0;
   }
+};
+
+const reset_Span = (spans) => {
+  for (const span of spans) {
+    span.innerHTML = '00';
+  }
+};
+
+const resetAllHandler = () => {
+  displayStartBtn();
+  timerOff();
+  reset_Span(main_timerSpans);
+  reset_Timer(main_timer);
   clearLaps();
 };
 checkSavedLaps();
--- lap.js
+++ lap.js
@@ -3,14 +3,20 @@ const clearLapsBtn = document.querySelector('.clear-lap');
 const ol = document.querySelector('.laps');
 
 let laps = [];
+let split_lap = [];
 
-const generateLapItem = (data) => {
+const renderLapItem = (ltd, mtd) => {
   const li = document.createElement('li');
-  li.innerHTML = data;
+  li.classList.add('lap-item');
+  const ltd_span = document.createElement('span');
+  const mtd_span = document.createElement('span');
+  ltd_span.innerHTML = ltd;
+  mtd_span.innerHTML = mtd;
+  li.append(ltd_span, mtd_span);
   ol.append(li);
 };
 
-const addLapHandler = () => {
+const generateStrLap = (timer) => {
   let str = '';
   for (const key in timer) {
     if (timer[key] < 10) {
@@ -19,26 +25,37 @@ const addLapHandler = () => {
       str += `${timer[key]}:`;
     }
   }
-  let data = str.slice(0, -1);
-  laps.push(data);
-  generateLapItem(data);
+  return str.slice(0, -1);
+};
+
+const addLapHandler = () => {
+  let lap_timer_data = generateStrLap(lap_timer);
+  let main_timer_data = generateStrLap(main_timer);
+
+  laps.push([lap_timer_data, main_timer_data]);
+  renderLapItem(lap_timer_data, main_timer_data);
   if (laps.length > 5) {
     ol.classList.add('scroll');
   }
   localStorage.setItem('lap-data', JSON.stringify(laps));
+  reset_Timer(lap_timer);
+  reset_Span(lap_timerSpans);
 };
 
 const checkSavedLaps = () => {
   const savedLaps = JSON.parse(localStorage.getItem('lap-data')) || [];
   if (savedLaps.length > 0) {
     laps = [...savedLaps];
-    savedLaps.forEach((data) => {
-      generateLapItem(data);
-    });
+    laps.forEach((data) => renderLapItem(data[0], data[1]));
+  }
+  if (laps.length > 5) {
+    ol.classList.add('scroll');
   }
 };
 
 const clearLaps = () => {
+  reset_Timer(lap_timer);
+  reset_Span(lap_timerSpans);
   localStorage.removeItem('lap-data');
   laps = [];
   ol.innerHTML = '';
